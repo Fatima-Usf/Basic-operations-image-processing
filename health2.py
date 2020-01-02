0f13d365f90b58794c4d73855ae62f7b46ee73af
Fatima-Usf <f.ucf.eva@gmail.com>
2020-01-02 20:09:27 +0100

    first operation, load nd resize an image

--- health2.py
+++ health2.py
@@ -1,3 +1,16 @@
+from PIL import Image
+from numpy import *
+from pylab import *
 # Basic operations 
 # 1- Resizing an image
+# A- Chargement de l'image 
+im = Image.open('health2.jpeg')
+im.show()
+image= im.resize((400,300))
+image.show()
 
+
+
+# 4-Converssion de couleurs au niveau de gris 
+im = Image.open('health2.jpeg').convert('L')
+im.show()""""" 
