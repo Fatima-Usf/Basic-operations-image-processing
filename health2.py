c151ab69ca802816b7150631cedd794c78b807eb
Fatima-Usf <f.ucf.eva@gmail.com>
2020-01-02 20:25:39 +0100

    Rotate an image

--- health2.py
+++ health2.py
@@ -2,15 +2,17 @@ from PIL import Image
 from numpy import *
 from pylab import *
 # Basic operations 
-# 1- Resizing an image
-# A- Chargement de l'image 
+# A- Load an image
 im = Image.open('health2.jpeg')
 im.show()
+# 1- Resizing an image
 image= im.resize((400,300))
 image.show()
-
+#2 - Rotate an image
+imrotated = im.rotate(45)
+imrotated.show()
 
 
 # 4-Converssion de couleurs au niveau de gris 
 im = Image.open('health2.jpeg').convert('L')
-im.show()""""" 
+im.show()
