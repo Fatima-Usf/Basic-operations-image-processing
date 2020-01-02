9a7a0b51f42a771e119bbc0baf4726d721c45ca3
Fatima-Usf <f.ucf.eva@gmail.com>
2020-01-02 20:44:12 +0100

    croping an image

--- health2.py
+++ health2.py
@@ -4,15 +4,21 @@ from pylab import *
 # Basic operations 
 # A- Load an image
 im = Image.open('health2.jpeg')
-im.show()
+#im.show()
 # 1- Resizing an image
 image= im.resize((400,300))
-image.show()
+#image.show()
 #2 - Rotate an image
 imrotated = im.rotate(45)
-imrotated.show()
+#imrotated.show()
+#3- Crop an image
 
+### Create a box to defin a region, the region is defined by a 4-tuple, where coordinates are (left, upper, right, lower).
+box = (40,30,90,70)
+### cropping a region from an image is done using the crop() method.
+region = im.crop(box)
+region.show()
 
 # 4-Converssion de couleurs au niveau de gris 
 im = Image.open('health2.jpeg').convert('L')
-im.show()
+#im.show()
